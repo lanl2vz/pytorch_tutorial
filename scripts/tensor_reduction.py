d29ac5833d697d9eabca10d4de246da297740a2e
lanl2vz <lanl2tz@hotmail.com>
2025-09-09 17:44:07 +0800

    Add initial project configuration and tensor reduction script

---
+++ scripts/tensor_reduction.py
@@ -0,0 +1,16 @@
+import torch
+
+scores = torch.rand( 3, 2, 4)
+print(f"Original shape: {scores.shape}")
+print(f"Tensor:\n{scores}")
+reduction_0 = torch.mean(scores, dim=0)
+print(f"Tensor reduction on the 0th dimension:\n{reduction_0}")
+print(f"Reduced shape: {reduction_0.shape}")
+
+
+scores = torch.rand( 3, 2, 4)
+print(f"Original shape: {scores.shape}")
+print(f"Tensor:\n{scores}")
+reduction_1 = torch.mean(scores, dim=1)
+print(f"Tensor reduction on the 0th dimension:\n{reduction_1}")
+print(f"Reduced shape: {reduction_1.shape}")
